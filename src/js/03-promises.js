8c1c4e2044aed487634cca4dab93a1db0b29db5b
aleksey-prokopchuk <prokop4huk@ukr.net>
2022-08-01 08:40:19 +0300

    fix

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,16 +1,16 @@
-const delayEl = document.querySelector('[name=delay]');
-const stepEl = document.querySelector('[name=step]');
-const amountEl = document.querySelector('[name=amount]');
-console.log(amountEl);
+// const delayEl = document.querySelector('[name=delay]');
+// const stepEl = document.querySelector('[name=step]');
+// const amountEl = document.querySelector('[name=amount]');
+// console.log(amountEl);
 
-let delay = delayEl.value;
-let stepEl = stepEl.value;
+// let delay = delayEl.value;
+// let stepEl = stepEl.value;
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }

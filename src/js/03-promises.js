ec64c444098860f05b8d4b69a274d5d563e607ef
aleksey-prokopchuk <prokop4huk@ukr.net>
2022-08-14 13:00:23 +0300

    Закінчив третю задачу

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,16 +1,55 @@
-// const delayEl = document.querySelector('[name=delay]');
-// const stepEl = document.querySelector('[name=step]');
-// const amountEl = document.querySelector('[name=amount]');
-// console.log(amountEl);
+import Notiflix from 'notiflix';
 
-// let delay = delayEl.value;
-// let stepEl = stepEl.value;
+const form = document.querySelector('.form');
 
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//   } else {
-//     // Reject
-//   }
-// }
+const firstDelay = document.querySelector('[name="delay"]');
+const delayStep = document.querySelector('[name="step"]');
+const amount = document.querySelector('[name="amount"]');
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault(5);
+  const firstDelayData = Number(firstDelay.value);
+  const delayStepData = Number(delayStep.value);
+  const amountDta = Number(amount.value);
+
+  for (let i = 0; i < amountDta; i += 1) {
+    const delay = firstDelayData + delayStepData * i;
+    const position = i + 1;
+    setTimeout(() => {
+      createPromise(position, delay)
+        .then(({ position, delay }) => {
+          Notiflix.Notify.success(
+            `✅ Fulfilled promise ${position} in ${delay}ms`,
+            {
+              timeout: 2000,
+            }
+          );
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.failure(
+            `❌ Rejected promise ${position} in ${delay}ms`,
+            {
+              timeout: 2000,
+            }
+          );
+        });
+    }, delay);
+  }
+}
+
+console.log(form);
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      // Fulfill
+      resolve({ position, delay });
+    } else {
+      // Reject
+      reject({ position, delay });
+    }
+  });
+}
